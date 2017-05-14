ba840d2761fc2c0248e2dbcb6b10fb217a8ef705
KarlChuang <karl0930@gmail.com>
2017-05-14 23:12:09 +0800

    All file OK

--- src/App.js
+++ src/App.js
@@ -5,23 +5,6 @@ import Block from './block';
 import Time from './Time';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-        <Block
-          state="bomp"
-          click={true}
-          flag={false}
-          key="2" numx={1} numy={1}
-          onClick={() => { console.log('Click'); }}
-          onContextMenu={() => { console.log('RightClick'); }}
-        />
-        <Time counter={0} start={true} />
-      </div>
-    );
-  }
-
-  /*
   constructor(props) {
     super(props);
     const TempArray = [];
@@ -39,7 +22,156 @@ class App extends Component {
       difficultyNum: 8,
       bombNumsRemain: 0,
     };
+    this.handleClick = this.handleClick.bind(this);
+    this.firstClick = this.firstClick.bind(this);
     this.ArrayMapping = this.ArrayMapping.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+    this.handleDifficultyChange = this.handleDifficultyChange.bind(this);
+    this.handleContextMenu = this.handleContextMenu.bind(this);
+  }
+  firstClick(numX, numY, TempArrayIn) {
+    const TempArray = TempArrayIn;
+    let bombNumsRemain = 0;
+    for (let i = 0; i < 20; i += 1) {
+      for (let j = 0; j < 20; j += 1) {
+        const random = Math.floor(Math.random() * 1000);
+        if (random % this.state.difficultyNum === 0) {
+          TempArray[i][j].state = 'bomp';
+          bombNumsRemain += 1;
+        }
+      }
+    }
+    for (let i = -1; i < 2; i += 1) {
+      for (let j = -1; j < 2; j += 1) {
+        if (numY + i >= 0 && numY + i < 20 && numX + j >= 0 && numX + j < 20) {
+          if (TempArray[numY + i][numX + j].state === 'bomp') {
+            TempArray[numY + i][numX + j].state = '0';
+            bombNumsRemain -= 1;
+          }
+        }
+      }
+    }
+    for (let i = 0; i < 20; i += 1) {
+      for (let j = 0; j < 20; j += 1) {
+        let count = 0;
+        if (TempArray[i][j].state !== 'bomp') {
+          for (let k = -1; k < 2; k += 1) {
+            for (let l = -1; l < 2; l += 1) {
+              if (i + k >= 0 && i + k < 20 && j + l >= 0 && j + l < 20 && TempArray[i + k][j + l].state === 'bomp') {
+                count += 1;
+              }
+            }
+          }
+          TempArray[i][j].state = count.toString();
+        }
+      }
+    }
+    return [TempArray, bombNumsRemain];
+  }
+  handleClick(numX, numY, e, count) {
+    let TempArray = this.state.BlockArray;
+    let newBombNumsRemain = this.state.bombNumsRemain;
+    let newStart = false;
+    let newCount = count;
+    if (TempArray[numY][numX].click === false) {
+      newStart = true;
+    }
+    if (count === 0) {
+      const re = (this.firstClick(numX, numY, TempArray));
+      TempArray = re[0];
+      newBombNumsRemain = re[1];
+    }
+    if (TempArray[numY][numX].click === false && TempArray[numY][numX].flag === false) {
+      TempArray[numY][numX].click = true;
+      newCount += 1;
+      if (TempArray[numY][numX].state === 'bomp') {
+        for (let i = 0; i < 20; i += 1) {
+          for (let j = 0; j < 20; j += 1) {
+            TempArray[numY][numX].state = 'bompClick';
+            TempArray[i][j].click = true;
+          }
+        }
+        newStart = false;
+        alert('lose!!!');
+      }
+      if (TempArray[numY][numX].state === '0') {
+        for (let k = -1; k <= 1; k += 1) {
+          for (let l = -1; l <= 1; l += 1) {
+            if (k !== 0 || l !== 0) {
+              if (numY + k >= 0 && numY + k < 20 && numX + l >= 0 && numX + l < 20) {
+                newCount = this.handleClick(numX + l, numY + k, e, newCount);
+              }
+            }
+          }
+        }
+      }
+    }
+    this.setState({
+      BlockArray: TempArray,
+      count: newCount,
+      start: newStart,
+      bombNumsRemain: newBombNumsRemain,
+    });
+    this.handleStop(newCount);
+    return newCount;
+  }
+  handleContextMenu(numX, numY, e) {
+    let newBombNumsRemain = this.state.bombNumsRemain;
+    e.preventDefault();
+    const TempArray = this.state.BlockArray;
+    if (TempArray[numY][numX].click === false) {
+      TempArray[numY][numX].flag = !(TempArray[numY][numX].flag);
+      if (TempArray[numY][numX].flag) {
+        newBombNumsRemain -= 1;
+      } else {
+        newBombNumsRemain += 1;
+      }
+      let newCount = this.state.count;
+      if (TempArray[numY][numX].state === 'bomp') {
+        if (TempArray[numY][numX].flag) {
+          newCount += 1;
+        } else {
+          newCount -= 1;
+        }
+      }
+      this.setState({
+        BlockArray: TempArray,
+        count: newCount,
+        bombNumsRemain: newBombNumsRemain,
+      });
+      this.handleStop(newCount);
+    }
+  }
+  handleDifficultyChange(e) {
+    const newDifficulty = e.target.value;
+    let newDifficultyNum = 8;
+    if (newDifficulty === 'medium') {
+      newDifficultyNum = 6;
+    }
+    if (newDifficulty === 'hard') {
+      newDifficultyNum = 4;
+    }
+    const TempArray = [];
+    for (let i = 0; i < 20; i += 1) {
+      TempArray.push([]);
+      for (let j = 0; j < 20; j += 1) {
+        TempArray[i].push({ state: '0', click: false, flag: false });
+      }
+    }
+    this.setState({
+      BlockArray: TempArray,
+      count: 0,
+      counter: 0,
+      start: false,
+      difficultyNum: newDifficultyNum,
+    });
+  }
+  handleStop(count) {
+    if (count === 400) {
+      if (confirm('Winnnnn!\nDo you want to start a new game?')) {
+        location.reload();
+      }
+    }
   }
   ArrayMapping(inputArray, numY) {
     const newArray = [];
@@ -57,6 +189,7 @@ class App extends Component {
     }
     return newArray;
   }
+
   render() {
     const line = [];
     for (let i = 0; i < 20; i += 1) {
@@ -104,7 +237,6 @@ class App extends Component {
       </div>
     );
   }
-  */
 }
 
 export default App;
